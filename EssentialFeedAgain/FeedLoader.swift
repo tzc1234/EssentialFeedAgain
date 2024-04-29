507b5abcbad002ed68c1110d057b97f242f3ab22
tzc1234 <peopli9997@gmail.com>
2024-04-29 13:14:21 +0100

    Create FeedLoader boundary

---
+++ EssentialFeedAgain/FeedLoader.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoader.swift
+//  EssentialFeedAgain
+//
+//  Created by Tsz-Lung on 29/04/2024.
+//
+
+import Foundation
+
+protocol FeedLoader {
+    func load(completion: @escaping (Result<[FeedImage], Error>) -> Void)
+}
