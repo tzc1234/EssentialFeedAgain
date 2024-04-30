71f8afa0767f9b3479514206376996ee34575cca
tzc1234 <peopli9997@gmail.com>
2024-04-30 17:25:22 +0100

    URLSessionHTTPClient.get requests form url

---
+++ EssentialFeedAgainTests/Feed API/URLSessionHTTPClientTests.swift
@@ -0,0 +1,68 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  EssentialFeedAgainTests
+//
+//  Created by Tsz-Lung on 30/04/2024.
+//
+
+import XCTest
+
+public final class URLSessionHTTPClient {
+    private let session: URLSession
+    
+    public init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        let task = session.dataTask(with: url) { data, response, error in
+            
+        }
+        task.resume()
+    }
+}
+
+final class URLSessionHTTPClientTests: XCTestCase {
+    func test_get_requestsFromURL() {
+        var configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLProtocolStub.self]
+        let session = URLSession(configuration: configuration)
+        let sut = URLSessionHTTPClient(session: session)
+        let url = URL(string: "https://request-url.com")!
+        
+        let exp = expectation(description: "Wait for request")
+        URLProtocolStub.observe { request in
+            XCTAssertEqual(request.url, url)
+            XCTAssertEqual(request.httpMethod, "GET")
+            exp.fulfill()
+        }
+        sut.get(from: url)
+        wait(for: [exp], timeout: 1)
+    }
+
+    // MARK: - Helpers
+    
+    private final class URLProtocolStub: URLProtocol {
+        private static var observer: ((URLRequest) -> Void)?
+        
+        static func observe(_ observer: @escaping (URLRequest) -> Void) {
+            Self.observer = observer
+        }
+        
+        override class func canInit(with request: URLRequest) -> Bool {
+            true
+        }
+        
+        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+            request
+        }
+        
+        override func startLoading() {
+            client?.urlProtocolDidFinishLoading(self)
+            
+            Self.observer?(request)
+        }
+        
+        override func stopLoading() {}
+    }
+}
