2629363580f142215df401466855d4b47e3529ea
tzc1234 <peopli9997@gmail.com>
2024-04-30 10:25:11 +0100

    Move HTTPClientSpy to its own file

---
+++ EssentialFeedAgainTests/Helpers/HTTPClientSpy.swift
@@ -0,0 +1,45 @@
+//
+//  HTTPClientSpy.swift
+//  EssentialFeedAgainTests
+//
+//  Created by Tsz-Lung on 30/04/2024.
+//
+
+import Foundation
+import EssentialFeedAgain
+
+final class HTTPClientSpy: HTTPClient {
+    private var messages = [(url: URL, completion: Completion)]()
+    var requestedURLs: [URL] {
+        messages.map(\.url)
+    }
+    
+    private struct Task: HTTPClientTask {
+        let afterCancel: () -> Void
+        
+        func cancel() {
+            afterCancel()
+        }
+    }
+    
+    private(set) var cancelCallCount = 0
+    
+    func get(from url: URL, completion: @escaping Completion) -> HTTPClientTask {
+        messages.append((url, completion))
+        return Task { [weak self] in
+            self?.cancelCallCount += 1
+        }
+    }
+    
+    func complete(with error: Error, at index: Int = 0) {
+        messages[index].completion(.failure(error))
+    }
+    
+    func complete(withStatusCode statusCode: Int, at index: Int = 0) {
+        messages[index].completion(.success((Data(), HTTPURLResponse(statusCode: statusCode))))
+    }
+    
+    func complete(with data: Data, at index: Int = 0) {
+        messages[index].completion(.success((data, HTTPURLResponse(statusCode: 200))))
+    }
+}
--- EssentialFeedAgainTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedAgainTests/RemoteFeedLoaderTests.swift
@@ -156,42 +156,6 @@ final class RemoteFeedLoaderTests: XCTestCase {
     private func anyNSError() -> NSError {
         NSError(domain: "any", code: 0)
     }
-    
-    private final class HTTPClientSpy: HTTPClient {
-        private var messages = [(url: URL, completion: Completion)]()
-        var requestedURLs: [URL] {
-            messages.map(\.url)
-        }
-        
-        private struct Task: HTTPClientTask {
-            let afterCancel: () -> Void
-            
-            func cancel() {
-                afterCancel()
-            }
-        }
-        
-        private(set) var cancelCallCount = 0
-        
-        func get(from url: URL, completion: @escaping Completion) -> HTTPClientTask {
-            messages.append((url, completion))
-            return Task { [weak self] in
-                self?.cancelCallCount += 1
-            }
-        }
-        
-        func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(.failure(error))
-        }
-        
-        func complete(withStatusCode statusCode: Int, at index: Int = 0) {
-            messages[index].completion(.success((Data(), HTTPURLResponse(statusCode: statusCode))))
-        }
-        
-        func complete(with data: Data, at index: Int = 0) {
-            messages[index].completion(.success((data, HTTPURLResponse(statusCode: 200))))
-        }
-    }
 }
 
 private extension [FeedImage] {
