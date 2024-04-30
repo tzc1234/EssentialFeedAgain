d595457c2b5955c13d737a2202faa5131914557d
tzc1234 <peopli9997@gmail.com>
2024-04-30 10:22:54 +0100

    RemoteFeedImageMapper.isOK function requires HTTPURLResponse instead of status code

--- EssentialFeedAgain/Feed API/RemoteFeedImageMapper.swift
+++ EssentialFeedAgain/Feed API/RemoteFeedImageMapper.swift
@@ -13,14 +13,14 @@ enum RemoteFeedImageMapper {
     }
     
     static func map(from data: Data, response: HTTPURLResponse) throws -> [RemoteFeedImage] {
-        guard isOK(response.statusCode), let root = try? JSONDecoder().decode(Root.self, from: data) else {
+        guard isOK(response), let root = try? JSONDecoder().decode(Root.self, from: data) else {
             throw RemoteFeedLoaderError.invalidData
         }
         
         return root.items
     }
     
-    private static func isOK(_ statusCode: Int) -> Bool {
-        statusCode == 200
+    private static func isOK(_ response: HTTPURLResponse) -> Bool {
+        response.statusCode == 200
     }
 }
