e1cf544d0572e9c89ea08e1d188e719f1ae3cfcf
tzc1234 <peopli9997@gmail.com>
2024-04-30 18:27:24 +0100

    Move URLSessionHTTPClient to production

--- EssentialFeedAgainTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedAgainTests/Feed API/URLSessionHTTPClientTests.swift
@@ -8,38 +8,6 @@
 import XCTest
 import EssentialFeedAgain
 
-public final class URLSessionHTTPClient: HTTPClient {
-    private let session: URLSession
-    
-    public init(session: URLSession) {
-        self.session = session
-    }
-    
-    public struct UnexpectedRepresentationError: Error {}
-    
-    private struct Wrapper: HTTPClientTask {
-        let task: URLSessionTask
-        
-        func cancel() {
-            task.cancel()
-        }
-    }
-    
-    public func get(from url: URL, completion: @escaping Completion) -> HTTPClientTask {
-        let task = session.dataTask(with: url) { data, response, error in
-            if let data, let httpResponse = response as? HTTPURLResponse {
-                completion(.success((data, httpResponse)))
-            } else if let error {
-                completion(.failure(error))
-            } else {
-                completion(.failure(UnexpectedRepresentationError()))
-            }
-        }
-        task.resume()
-        return Wrapper(task: task)
-    }
-}
-
 final class URLSessionHTTPClientTests: XCTestCase {
     func test_get_requestsFromURL() {
         let sut = makeSUT()
