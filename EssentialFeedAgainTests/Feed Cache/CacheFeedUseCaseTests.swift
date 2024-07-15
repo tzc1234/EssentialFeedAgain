ad8937df1b4d1504cfe4e21b26c0c3b54b83bd36
tzc1234 <peopli9997@gmail.com>
2024-07-15 17:51:20 +0100

    Move FeedStoreSpy to its own file

--- EssentialFeedAgainTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedAgainTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -28,36 +28,6 @@ protocol FeedStore {
     func insert(_ feed: [FeedImage], timestamp: Date) async throws
 }
 
-final class FeedStoreSpy: FeedStore {
-    typealias DeletionStub = Result<Void, Error>
-    typealias InsertionStub = Result<Void, Error>
-    
-    enum Message: Equatable {
-        case deleteCachedFeed
-        case insert([FeedImage], Date)
-    }
-    
-    private(set) var messages = [Message]()
-    
-    private var deletionStubs: [DeletionStub]
-    private var insertionStubs: [InsertionStub]
-    
-    init(deletionStubs: [DeletionStub], insertionStubs: [InsertionStub]) {
-        self.deletionStubs = deletionStubs
-        self.insertionStubs = insertionStubs
-    }
-    
-    func deleteCachedFeed() async throws {
-        messages.append(.deleteCachedFeed)
-        try deletionStubs.removeFirst().get()
-    }
-    
-    func insert(_ feed: [FeedImage], timestamp: Date) async throws {
-        messages.append(.insert(feed, timestamp))
-        try insertionStubs.removeFirst().get()
-    }
-}
-
 final class CacheFeedUseCaseTests: XCTestCase {
     func test_init_doesNotNotifyStoreUponInit() {
         let (_, store) = makeSUT()
