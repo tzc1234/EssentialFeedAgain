973a325a901274c4005e682c4bed137b3bb93880
tzc1234 <peopli9997@gmail.com>
2024-07-16 15:03:19 +0100

    LocalFeedLoader does not notify store upon init (Before validating the cached feed)

---
+++ EssentialFeedAgainTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -0,0 +1,31 @@
+//
+//  ValidateFeedCacheUseCaseTests.swift
+//  EssentialFeedAgainTests
+//
+//  Created by Tsz-Lung on 16/07/2024.
+//
+
+import XCTest
+import EssentialFeedAgain
+
+final class ValidateFeedCacheUseCaseTests: XCTestCase {
+    func test_init_doesNotNotifyStoreUponInit() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertTrue(store.messages.isEmpty)
+    }
+
+    // MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init,
+                         deletionStubs: [FeedStoreSpy.DeletionStub] = [],
+                         insertionStubs: [FeedStoreSpy.InsertionStub] = [],
+                         file: StaticString = #filePath,
+                         line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy(deletionStubs: deletionStubs, insertionStubs: insertionStubs, retrievalStubs: [])
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+}
