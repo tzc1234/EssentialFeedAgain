47d0eea0e81e8b95cd846f3af687557c290cb81c
tzc1234 <peopli9997@gmail.com>
2024-07-16 15:33:38 +0100

    Extract date test helpers to its own file

--- EssentialFeedAgainTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedAgainTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -136,19 +136,3 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
         .success((feed, timestamp))
     }
 }
-
-extension Date {
-    func minusMaxCacheAgeInDays() -> Date {
-        adding(days: -feedCacheMaxAgeInDays)
-    }
-    
-    private var feedCacheMaxAgeInDays: Int { 7 }
-    
-    func adding(days: Int, calendar: Calendar = Calendar(identifier: .gregorian)) -> Date {
-        calendar.date(byAdding: .day, value: days, to: self)!
-    }
-    
-    func adding(seconds: TimeInterval) -> Date {
-        self + seconds
-    }
-}
