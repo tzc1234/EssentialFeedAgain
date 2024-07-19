c4c7cc424ccfe901a156bf8ca942f5d597cf66ba
tzc1234 <peopli9997@gmail.com>
2024-07-19 10:02:30 +0100

    Create FeedStoreSpecs that must be implemented by any FeedStore test implementation to guarantee it meets spec

--- EssentialFeedAgainTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedAgainTests/Feed Cache/CodableFeedStoreTests.swift
@@ -8,38 +8,7 @@
 import XCTest
 import EssentialFeedAgain
 
-protocol FeedStoreSpecs {
-    func test_retrieve_deliversEmptyOnEmptyCache() async throws
-    func test_retrieveTwice_hasNoSideEffectsOnEmptyCache() async throws
-    func test_retrieve_deliversFoundValuesOnNonEmptyCache() async throws
-    func test_retrieveTwice_hasNoSideEffectsOnNonEmptyCache() async throws
-    
-    func test_insert_deliversNoErrorOnEmptyCache() async
-    func test_insert_deliversNoErrorOnNonEmptyCache() async throws
-    func test_insert_overridesPreviouslyInsertedCacheValues() async throws
-    
-    func test_delete_deliversNoErrorOnEmptyCache() async
-    func test_delete_hasNoSideEffectsOnEmptyCache() async throws
-    func test_delete_deliversNoErrorOnNonEmptyCache() async throws
-    func test_delete_emptiesPreviouslyInsertedCache() async throws
-}
-
-protocol FailableRetrieveFeedStoreSpecs {
-    func test_retrieve_deliversErrorOnRetrievalError() async
-    func test_retrieveTwice_hasNoSideEffectsOnRetrievalError() async
-}
-
-protocol FailableInsertFeedStoreSpecs {
-    func test_insert_deliversErrorOnInsertionError() async
-    func test_insert_hasNoSideEffectsOnInsertionError() async throws
-}
-
-protocol FailableDeleteFeedStoreSpecs {
-    func test_delete_deliversErrorOnDeletionError() async
-    func test_delete_hasNoSideEffectsOnDeletionError() async
-}
-
-final class CodableFeedStoreTests: XCTestCase {
+final class CodableFeedStoreTests: XCTestCase, FailableFeedStore {
     override func tearDown() async throws {
         try await super.tearDown()
         
