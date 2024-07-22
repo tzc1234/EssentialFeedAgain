a1fd824cd9a07844d7552c5aeb691072b0d079f4
tzc1234 <peopli9997@gmail.com>
2024-07-22 14:54:40 +0100

    Add empty core data feed store specs

---
+++ EssentialFeedAgainTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,56 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeedAgainTests
+//
+//  Created by Tsz-Lung on 22/07/2024.
+//
+
+import XCTest
+
+final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache() async throws {
+        
+    }
+    
+    func test_retrieveTwice_hasNoSideEffectsOnEmptyCache() async throws {
+        
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() async throws {
+        
+    }
+    
+    func test_retrieveTwice_hasNoSideEffectsOnNonEmptyCache() async throws {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() async {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() async throws {
+        
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCacheValues() async throws {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() async {
+        
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() async throws {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() async throws {
+        
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() async throws {
+        
+    }
+    
+    
+}
