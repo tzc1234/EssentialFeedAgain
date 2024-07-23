7b3ef5e13e068d15bc220f5268708995246e9e55
tzc1234 <peopli9997@gmail.com>
2024-07-23 09:48:52 +0100

    Extract ManagedFeedImage to its own file

--- EssentialFeedAgain/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeedAgain/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -97,16 +97,3 @@ extension CoreDataFeedStore {
         return container
     }
 }
-
-@objc(ManagedFeedImage)
-final class ManagedFeedImage: NSManagedObject {
-    @NSManaged var id: UUID
-    @NSManaged var imageDescription: String?
-    @NSManaged var location: String?
-    @NSManaged var url: URL
-    @NSManaged var cache: ManagedCache
-    
-    var local: LocalFeedImage {
-        LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
-    }
-}
