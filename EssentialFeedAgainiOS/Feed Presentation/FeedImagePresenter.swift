ede3e072eebaf847f320ef99c1b5f29f0fe9175f
tzc1234 <peopli9997@gmail.com>
2024-07-30 15:12:13 +0100

    Move FeedImageViewModel to its own file

--- EssentialFeedAgainiOS/Feed Presentation/FeedImagePresenter.swift
+++ EssentialFeedAgainiOS/Feed Presentation/FeedImagePresenter.swift
@@ -8,18 +8,6 @@
 import Foundation
 import EssentialFeedAgain
 
-struct FeedImageViewModel<Image> {
-    let description: String?
-    let location: String?
-    let image: Image?
-    let isLoading: Bool
-    let shouldRetry: Bool
-    
-    var hasLocation: Bool {
-        location != nil
-    }
-}
-
 protocol FeedImageView {
     associatedtype Image
     
