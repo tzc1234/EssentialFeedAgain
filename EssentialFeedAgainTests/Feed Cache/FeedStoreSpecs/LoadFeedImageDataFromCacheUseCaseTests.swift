1ee33bafb8be1725d78d3be6e4cc300ef548c1a3
tzc1234 <peopli9997@gmail.com>
2024-08-06 09:48:09 +0100

    LocalFeedImageDataLoader.loadImageData fails on store error

--- EssentialFeedAgainTests/Feed Cache/FeedStoreSpecs/LoadFeedImageDataFromCacheUseCaseTests.swift
+++ EssentialFeedAgainTests/Feed Cache/FeedStoreSpecs/LoadFeedImageDataFromCacheUseCaseTests.swift
@@ -14,13 +14,18 @@ final class LocalFeedImageDataLoader {
         self.store = store
     }
     
-    func loadImageData(from url: URL) {
-        store.retrieve(dataFor: url)
+    enum Error: Swift.Error {
+        case failed
+    }
+    
+    func loadImageData(from url: URL) async throws {
+        try? store.retrieve(dataFor: url)
+        throw Error.failed
     }
 }
 
 protocol FeedImageDataStore {
-    func retrieve(dataFor url: URL)
+    func retrieve(dataFor url: URL) throws
 }
 
 final class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
@@ -30,20 +35,30 @@ final class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
         XCTAssertTrue(store.messages.isEmpty)
     }
     
-    func test_loadImageData_requestsCachedDataForURL() {
+    func test_loadImageData_requestsCachedDataForURL() async {
         let (sut, store) = makeSUT()
         let url = URL(string: "https://a-given-url.com")!
         
-        sut.loadImageData(from: url)
+        try? await sut.loadImageData(from: url)
         
         XCTAssertEqual(store.messages, [.retrieve(dataFor: url)])
     }
     
+    func test_loadImageData_failsOnStoreError() async {
+        let retrievalError = anyNSError()
+        let (sut, store) = makeSUT(retrieveStubs: [.failure(retrievalError)])
+        
+        await assertThrowsError(try await sut.loadImageData(from: anyURL())) { error in
+            XCTAssertEqual(error as? LocalFeedImageDataLoader.Error, .failed)
+        }
+    }
+    
     // MARK: - Helpers
     
-    private func makeSUT(file: StaticString = #filePath,
+    private func makeSUT(retrieveStubs: [StoreSpy.RetrieveStub] = [],
+                         file: StaticString = #filePath,
                          line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: StoreSpy) {
-        let store = StoreSpy()
+        let store = StoreSpy(retrieveStubs: retrieveStubs)
         let sut = LocalFeedImageDataLoader(store: store)
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
@@ -51,14 +66,25 @@ final class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
     }
     
     final class StoreSpy: FeedImageDataStore {
+        typealias RetrieveStub = Result<Void, Error>
+        
         enum Message: Equatable {
             case retrieve(dataFor: URL)
         }
         
         private(set) var messages = [Message]()
+        private var retrieveStubs = [RetrieveStub]()
         
-        func retrieve(dataFor url: URL) {
+        init(retrieveStubs: [RetrieveStub]) {
+            self.retrieveStubs = retrieveStubs
+        }
+        
+        func retrieve(dataFor url: URL) throws {
             messages.append(.retrieve(dataFor: url))
+            
+            guard !retrieveStubs.isEmpty else { return }
+            
+            try retrieveStubs.removeFirst().get()
         }
     }
 }
