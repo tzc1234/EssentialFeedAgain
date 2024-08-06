97edc79dfe5e12af4303b18044d30ac10d79db66
tzc1234 <peopli9997@gmail.com>
2024-08-06 09:38:13 +0100

    LocalFeedImageDataLoader.loadImageData requests cached data for URL

--- EssentialFeedAgainTests/Feed Cache/FeedStoreSpecs/LoadFeedImageDataFromCacheUseCaseTests.swift
+++ EssentialFeedAgainTests/Feed Cache/FeedStoreSpecs/LoadFeedImageDataFromCacheUseCaseTests.swift
@@ -8,11 +8,19 @@
 import XCTest
 
 final class LocalFeedImageDataLoader {
-    private let store: Any
+    private let store: FeedImageDataStore
     
-    init(store: Any) {
+    init(store: FeedImageDataStore) {
         self.store = store
     }
+    
+    func loadImageData(from url: URL) {
+        store.retrieve(dataFor: url)
+    }
+}
+
+protocol FeedImageDataStore {
+    func retrieve(dataFor url: URL)
 }
 
 final class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
@@ -22,18 +30,35 @@ final class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
         XCTAssertTrue(store.messages.isEmpty)
     }
     
+    func test_loadImageData_requestsCachedDataForURL() {
+        let (sut, store) = makeSUT()
+        let url = URL(string: "https://a-given-url.com")!
+        
+        sut.loadImageData(from: url)
+        
+        XCTAssertEqual(store.messages, [.retrieve(dataFor: url)])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath,
-                         line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedStoreSpy) {
-        let store = FeedStoreSpy()
+                         line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: StoreSpy) {
+        let store = StoreSpy()
         let sut = LocalFeedImageDataLoader(store: store)
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
     }
     
-    final class FeedStoreSpy {
-        private(set) var messages = [Any]()
+    final class StoreSpy: FeedImageDataStore {
+        enum Message: Equatable {
+            case retrieve(dataFor: URL)
+        }
+        
+        private(set) var messages = [Message]()
+        
+        func retrieve(dataFor url: URL) {
+            messages.append(.retrieve(dataFor: url))
+        }
     }
 }
