d5242e2593b1c61086bc2f0f7ed21cc45c476d6b
tzc1234 <peopli9997@gmail.com>
2024-08-07 09:35:57 +0100

    Add empty EssentialApp project

---
+++ EssentialAppAgain/EssentialAppAgain/AppDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  AppDelegate.swift
+//  EssentialAppAgain
+//
+//  Created by Tsz-Lung on 07/08/2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {}
---
+++ EssentialAppAgain/EssentialAppAgain/SceneDelegate.swift
@@ -0,0 +1,10 @@
+//
+//  SceneDelegate.swift
+//  EssentialAppAgain
+//
+//  Created by Tsz-Lung on 07/08/2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {}
