8749d94408f64dfa98506f30241070978d817d87
tzc1234 <peopli9997@gmail.com>
2024-08-12 14:33:40 +0100

    Intercept HTTP requests with canned response during UI tests to eliminate network flakiness - We can now run UI tests without network connection.

--- EssentialAppAgain/EssentialAppAgainUIAcceptanceTests/EssentialAppAgainUIAcceptanceTests.swift
+++ EssentialAppAgain/EssentialAppAgainUIAcceptanceTests/EssentialAppAgainUIAcceptanceTests.swift
@@ -10,13 +10,12 @@ import XCTest
 final class EssentialAppAgainUIAcceptanceTests: XCTestCase {
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
         let app = XCUIApplication()
-        app.launchArguments = ["-reset"]
+        app.launchArguments = ["-reset", "-connectivity", "online"]
         
         app.launch()
         
         let feedCells = app.cells.matching(identifier: "feed-image-cell")
-        _ = feedCells.firstMatch.waitForExistence(timeout: 1)
-        XCTAssertEqual(feedCells.count, 22)
+        XCTAssertEqual(feedCells.count, 2)
         
         let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(firstImage.exists)
@@ -24,19 +23,15 @@ final class EssentialAppAgainUIAcceptanceTests: XCTestCase {
     
     func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
         let onlineApp = XCUIApplication()
-        onlineApp.launchArguments = ["-reset"]
+        onlineApp.launchArguments = ["-reset", "-connectivity", "online"]
         onlineApp.launch()
-        
-        let feedCells = onlineApp.cells.matching(identifier: "feed-image-cell")
-        _ = feedCells.firstMatch.waitForExistence(timeout: 1)
         
         let offlineApp = XCUIApplication()
         offlineApp.launchArguments = ["-connectivity", "offline"]
         offlineApp.launch()
         
         let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
-        _ = cachedFeedCells.firstMatch.waitForExistence(timeout: 1)
-        XCTAssertEqual(cachedFeedCells.count, 22)
+        XCTAssertEqual(cachedFeedCells.count, 2)
         
         let firstCachedImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(firstCachedImage.exists)
